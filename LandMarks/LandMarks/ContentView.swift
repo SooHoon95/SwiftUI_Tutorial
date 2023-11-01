2c44b82e0c7c59504acd07a6b2df5eb11394ae52
SooHoon <dkehskeh@gmail.com>
2023-11-01 21:23:14 +0900

    project 생성

---
+++ LandMarks/LandMarks/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  LandMarks
+//
+//  Created by 최수훈 on 2023/10/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LandMarks/LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by 최수훈 on 2023/10/31.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
