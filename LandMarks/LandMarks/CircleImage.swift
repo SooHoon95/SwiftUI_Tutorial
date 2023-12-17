8fc1ce8127b0ab163710c5c38d56d3cd23bb62a8
SooHoon <dkehskeh@gmail.com>
2023-12-17 20:54:00 +0900

    [ADD] :heavy_plus_sign:  Custim Image
    Custom Image
    - clipShape
    - overlay { Circle () }
    - shadow

---
+++ LandMarks/LandMarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  LandMarks
+//
+//  Created by 최수훈 on 12/17/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.gray, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
